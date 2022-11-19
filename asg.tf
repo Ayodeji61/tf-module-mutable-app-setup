3df3e2a0795afb3769d971f746b277bbfa0953a9
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-19 15:58:33 -0700

    Updating

--- asg.tf
+++ asg.tf
@@ -5,6 +5,7 @@ resource "aws_launch_template" "launch-template" {
 }
 
 resource "aws_autoscaling_group" "asg" {
+  name              = "${var.env}-${var.name}-asg"
   desired_capacity = var.min_size
   max_size         = var.max_size
   min_size         = var.min_size
