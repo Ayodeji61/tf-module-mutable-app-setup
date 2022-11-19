2c2911895641958363bf0f5f1703a07b58e5b71d
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-19 16:44:47 -0700

    Updating

--- asg.tf
+++ asg.tf
@@ -2,6 +2,10 @@ resource "aws_launch_template" "launch-template" {
   name           = "${var.env}-${var.name}-lt"
   image_id      = data.aws_ami.centos-8-ami.image_id
   instance_type = var.instance_type
+  user_data = base64encode(templatefile("${path.module}/ansible-pull.sh", {
+    COMPONENT   = var.name
+    ENV         = var.env
+  } ))
 }
 
 resource "aws_autoscaling_group" "asg" {
