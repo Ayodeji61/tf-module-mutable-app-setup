87a6a5b5833480a65c813c611e17f5c9284a2847
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-19 15:34:06 -0700

    Updating

--- asg.tf
+++ asg.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "launch-template" {
+resource "aws_launch_template" "launch-template" {
   name_prefix   = "${var.env}-${var.name}-lt"
   image_id      = data.aws_ami.centos-8-ami.image_id
   instance_type = var.instance_type
