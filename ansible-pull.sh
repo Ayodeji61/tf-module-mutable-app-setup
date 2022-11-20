21247e5e308c2d56b7ea5bb110f12d8c7fac4448
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-19 17:54:57 -0700

    Updating

--- ansible-pull.sh
+++ ansible-pull.sh
@@ -4,4 +4,4 @@ LOG=/opt/user-data-$$.log
 yum install python39-devel -y &>>$LOG
 pip3.9 install ansible botocore boto3 &>>$LOG
 
-ansible-pull -i localhost, -U https://github.com/Ayodeji61/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV}
+ansible-pull -i localhost, -U https://github.com/Ayodeji61/roboshop-ansible roboshop.yml -e ROLE_NAME=${COMPONENT} -e ENV=${ENV} &>>$LOG
